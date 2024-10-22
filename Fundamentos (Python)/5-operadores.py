c4d0460de8375e1b140472ff352da979754ff95b
Alex Henrique <85684358+alexshenrique@users.noreply.github.com>
2024-10-22 17:25:39 -0300

    Update 5-operadores.py

--- Fundamentos (Python)/5-operadores.py
+++ Fundamentos (Python)/5-operadores.py
@@ -10,9 +10,17 @@ mult = num1 * num2
 mod = num1 % num2
 exp = num1 ** num2
 
-print(sum)
-print(sub)
-print(div)
-print(mult)
-print(mod)
-print(exp)
+print(f"Resto da divisão: de {num1} por {num2} é {mod}")
+print(f"Potencia: de {num1} elevado a {num2} é {exp}")
+
+#comparação
+
+bigger = num1 > num2
+smaller = num1 < num2
+equal = num1 == num2
+different = num1!= num2
+bigger_equal = num1 >= num2
+smaller_equal = num1 <= num2
+
+print(f"Os números {num1} e {num2} são iguais? {equal}")
+print(f"O número {num1} é maior ou igual a {num2}? {bigger_equal}")

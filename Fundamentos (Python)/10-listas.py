5ec0f2cbf52db5717b384e587daa719c73ee4c73
Alex Henrique <85684358+alexshenrique@users.noreply.github.com>
2024-10-23 17:24:57 -0300

    Atualizações

---
+++ Fundamentos (Python)/10-listas.py
@@ -0,0 +1,12 @@
+gameFifa= ['Fifa 24', 2022, 90.50, True]
+print(gameFifa)
+
+gameList = ["Resident Evil 4", "Star Wars Jedi Survivor",
+            "The Legend of Zelda", "Red Dead Redemption 2"]
+print(gameList)
+
+# 1 - Buscar os dois primeiros itens da lista
+print(gameList[0:2])
+
+# 2 - Buscar o ultimo item da lista
+print(gameList[-1])
